23d028c5c0078bd91ebb61258dd67bfb8e5bd08f
Baymurat <baymurat.orazov@mail.ru>
2023-06-21 20:38:14 +0500

    feat: implement indecate active ellipse

--- src/js/script.js
+++ src/js/script.js
@@ -14,18 +14,40 @@ btnRight.addEventListener('click', () => {
   })
 })
 
-const banners = document.querySelector('.slider .slider__banners')
+const banners = document.querySelectorAll('.slider .slider__banner-content')
+const ellipses = document.querySelectorAll('.slider .slider__control-ellipse')
+const map = new Map()
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      ellipses.forEach((ellipse) => {
+        ellipse.classList.remove('active-ellipse')
+      })
+
+      const target = map.get(entry.target)
+      target.classList.add('active-ellipse')
+    }
+  })
+})
+
+banners.forEach((banner, index) => {
+  map.set(banner, ellipses[index])
+  observer.observe(banner)
+})
+
+const bannersContainer = document.querySelector('.slider .slider__banners')
 const bannersCount = document.querySelectorAll('.slider .slider__banner').length
 let count = 1
 
 const intervalId = setInterval(() => {
   if (count === bannersCount) {
     count = 1
-    banners.scrollTo({
+    bannersContainer.scrollTo({
       left: 0
     })
   } else {
-    banners.scrollBy({
+    bannersContainer.scrollBy({
       left: window.innerWidth
     })
     count++
